3b7b3e03e5c82d0d9ba48417c741f1ecde3098d9
Başar Oğuz <baoguz@ku.edu.tr>
2016-07-07 11:06:28 +0300

    Create EmotionsViewController.

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Başar Oğuz on 07/07/16.
+//  Copyright © 2016 basaroguz. All rights reserved.
+//
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+    
+    
+}
